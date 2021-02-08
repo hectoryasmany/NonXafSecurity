d0da6b4b9ed17986d271439bc0329b93b6164d7e
jcolumbiedev <jcolumbiedev@gmail.com>
2021-02-08 18:47:29 -0500

    Add project files.

---
+++ WebForms/VB/Global.asax.vb
@@ -0,0 +1,20 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Optimization
+Imports System.Web.Routing
+Imports System.Web.Security
+Imports System.Web.SessionState
+
+Namespace WebFormsApplication
+    Public Class [Global]
+        Inherits HttpApplication
+
+        Private Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
+            ' Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes)
+            BundleConfig.RegisterBundles(BundleTable.Bundles)
+        End Sub
+    End Class
+End Namespace
